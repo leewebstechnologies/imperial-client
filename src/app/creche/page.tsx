f2081846eaff77ff3f5d607a9e7f28e5d8710f96
leewebstechnologies.ali <leewebstechnologies.aliuajibade317@gmail.com>
2026-01-15 09:26:31 -0800

    change h1 color of the banner to white

--- src/app/creche/page.tsx
+++ src/app/creche/page.tsx
@@ -1,8 +1,27 @@
+import Image from "next/image";
 import "./creche.css";
 
 const Creche = () => {
   return (
-    <div>Creche</div>
+    <>
+        <section className="hero">
+            <Image
+              src="/images/creche/banner.jpg"
+              alt="Elementary Classroom"
+              fill
+              priority
+              className="heroImage"
+            />
+    
+            <div className="heroOverlay">
+              <h1>Creche</h1>
+              <span className="underline"></span>
+            </div>
+          </section>
+          <section>
+          
+          </section>
+    </>
   )
 }
 export default Creche
